a0da1815bfc7cd69511daca282805c67ea8bc651
Marko Vujic <mavu1010@enjoying.rs>
2023-04-01 11:31:52 +0200

    Cancelation token

--- Parallel_Output/Program.cs
+++ Parallel_Output/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+internal class Program
+{
+    
+    private static void Main(string[] args)
+    {
+        var cts = new CancellationTokenSource();
+
+        var token = cts.Token;
+
+        var task = new Task(() =>
+        {
+            int i = 0;
+
+            while (true) 
+            {
+                Console.WriteLine($"taks {i++}");
+            }
+        }, token);
+
+        task.Start();
+
+        Console.ReadKey();
+        cts.Cancel();
+
+        Console.ReadKey();
+    }
+}
